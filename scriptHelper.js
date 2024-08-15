64b3f3066412c0b6a534437e9d8600f45aea97f9
Latharagav <tgpremlatha@gmail.com>
2024-08-14 21:59:30 -0500

    input validation

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,6 +2,22 @@
 
 require('cross-fetch/polyfill');
 
+/*window.addEventListener("load", function(){
+    let form = document.getElementById("launchForm");
+    console.log("done");
+    form.addEventListener("submit", function(event){
+        alert("Submit clicked");
+        console.log("submit clicked");
+    })
+   form.addEventListener("submit",function(event){
+        let inputPilotName = document.querySelector(input[name=pilotName]);
+            let inputCoPilotName = document.querySelector(input[name=copilotName]);
+            let inputFuelLevel = document.querySelector(input[name=fuelLevel]);
+            let inputCargoMass = document.querySelector(input[name=cargoMass]);
+
+    });
+})*/
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
     /*
@@ -18,11 +34,38 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  }
  
  function validateInput(testInput) {
-    
+    if(testInput === ""){
+        return "Empty";
+    }
+    else if(isNaN(testInput)){
+        return "Not a number";
+    }
+    else {
+        return "Is a number";
+    }
  }
  
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    let pilotResult = validateInput(pilot);
+    let coPilotResult = validateInput(copilot);
+    let fuelLevelResult = validateInput(fuelLevel);
+    let cargoLevelResult = validateInput(cargoLevel);
+    let pilotStatus = document.getElementById("pilotStatus");
+    let copilotStatus = document.getElementById("copilotStatus");
+    let fuelStatus = document.getElementById("fuelStatus");
+    let cargoStatus = document.getElementById("cargoStatus");
+
+    list.style.visibility = "visible";
+    console.log("changed style");
+    if (pilotResult === "Not a number" && coPilotResult === "Not a number" && 
+    fuelLevelResult === "Is a number" && cargoLevelResult === "Is a number") {
+        if(fuelLevelResult > 10000){
+            fuelStatus.innerHTML = "Fuel level high enough for launch";      
+        }
+        if(cargoLevelResult >)
     
+    }
+
  }
  
  async function myFetch() {
