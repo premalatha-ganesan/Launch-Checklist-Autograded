af9abeb21c205f2573c4c4bdf2c030558e3e33c5
Latharagav <tgpremlatha@gmail.com>
2024-08-18 17:50:28 -0500

    Launch Checklist- GA5

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
 // Write your JavaScript code here!
 
-//const { myFetch } = require("./scriptHelper");
-
-window.addEventListener("load", function() {
-    console.log("planet response");
-
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    // let listedPlanetsResponse = myFetch();
-    // console.log(listedPlanetsResponse);
-    // console.log("Listed Planets Response : " + listedPlanetsResponse);
-    // listedPlanetsResponse.then(function (result) {
-    //    listedPlanets = result;
-    //   console.log(listedPlanets);
-    //  }).then(function () {
-    //     console.log(listedPlanets);
-    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    //     let selectedPlanet = pickPlanet(listedPlanets);
-    //     console.log("Selected Planet :" + selectedPlanet);
-    //  })
-  });
-
-
-    window.addEventListener("load", function(){
-
-        console.log("Page Loaded");
-       
+  
+  window.addEventListener("load", function() {
         let listedPlanets;
         let listedPlanetsResponse = myFetch();
-        console.log("Planets response " + listedPlanetsResponse);
 
         listedPlanetsResponse.then(function(result) {
-            console.log("Result" + result);
             listedPlanets = result;
         }).then(function () {
             let selectedPlanet = pickPlanet(listedPlanets);
-            console.log("Selected Planet :" + selectedPlanet.name);
-            
-            /*
-            addDestinationInf
-            */
             addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter,selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image );
-            //document.getElementById("missionTarget").innerHTML = selectedPlanet.name;
         });
 
         let form = document.getElementById("launchForm");
@@ -51,19 +19,14 @@ window.addEventListener("load", function() {
         let inputCargoMass = document.getElementById("launchForm").querySelector("input[name=cargoMass]");
         let list = document.getElementById("launchStatusCheck").querySelector("#faultyItems");
         
-        form.addEventListener("submit", function(event){
-            console.log("submit success");
-            console.log(inputPilotName.value);
-            console.log(inputCoPilotName.value);
-            console.log(inputFuelLevel.value);
-            console.log(inputCargoMass.value);   
-         
-            if(inputPilotName.value === "" || inputCoPilotName.value === "" || inputFuelLevel.value === "" || 
-            inputCargoMass.value === ""){
+        form.addEventListener("submit", function(event) {
+            if(inputPilotName.value === "" 
+                    || inputCoPilotName.value === "" 
+                    || inputFuelLevel.value === "" 
+                    || inputCargoMass.value === "") {
                 alert("All fields required");
-              //  event.preventDefault();
             }
-            else{
+            else {
                 formSubmission(document,list, inputPilotName.value, inputCoPilotName.value, inputFuelLevel.value, inputCargoMass.value);
             }
             event.preventDefault();
